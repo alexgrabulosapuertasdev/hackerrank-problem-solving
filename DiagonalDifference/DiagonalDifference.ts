4be2b52cdfe582264ab7fd82c447594a840fe81f
Àlex Grabulosa Puertas <alexgrabulosapuertas9@gmail.com>
2023-07-12 19:36:28 +0200

    Feat: add DiagonalDifference

---
+++ DiagonalDifference/DiagonalDifference.ts
@@ -0,0 +1,62 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr: number[][]): number {
+    // Write your code here
+    const n = arr.length;
+    let num1 = 0;
+    
+    for (let i = 0; i < n; i++) {
+        num1 += arr[i][i];
+        num1 -= arr[n - i - 1][i]
+    }
+    
+    return Math.abs(num1);
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    let arr: number[][] = Array(n);
+
+    for (let i: number = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result: number = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
